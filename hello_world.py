45fdc4fdb6eab498b2105789cdd14d9d7a510e14
Athif-gitt <athiffaisal55@gmail.com>
2025-11-05 12:44:50 +0530

    updation

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
-words = ["hi", "hello", "world"]
-find_length = list(map(lambda w: len(w), words))
-print(find_length)
+# words = ["hi", "hello", "world"]
+# find_length = list(map(lambda w: len(w), words))
+# print(find_length)
+#----------------
+
+import Example
+add_numbers = Example.add(1, 2)
+print(add_numbers)
