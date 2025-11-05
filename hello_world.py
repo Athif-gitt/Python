f686402cf21b43091c8d99519c2d4469d6307cbf
Athif-gitt <athiffaisal55@gmail.com>
2025-11-05 20:47:51 +0530

    next updation

--- hello_world.py
+++ hello_world.py
@@ -2,17 +2,53 @@
 # find_length = list(map(lambda w: len(w), words))
 # print(find_length)
 #----------------
+from logging import raiseExceptions
 
 # import Example
 # add_numbers = Example.add(1, 2)
 # print(add_numbers)
 
-try:
-    numerator = 10
-    denomination = 0
-    result = numerator / denomination
-    print(result)
+# try:
+#     numerator = 10
+#     denomination = 0
+#     result = numerator / denomination
+#     print(result)
+#
+#
+# except :
+#     print("error")
+
+# try:
+#     fruits = ["apple", "banana", "litchi"]
+#     print(fruits[4])
+# except IndexError as e:
+#     print("Error", e)
+
+# try:
+#     def check_age(age):
+#         if age <= 18:
+#             raise Exception("under_aged")
+#         else:
+#             print("aged")
+# except:
+#     print("error")
+# finally:
+#     print("execution completed")
+#
+# check_age(19)
+
+# a = 20
+# print(dir())
+
+# a = "python"
+# updated = a[1: len(a)]
+# print(updated.replace("t", ""))
 
 
-except:
-    print("error")
+
+
+
+
+
+
+
